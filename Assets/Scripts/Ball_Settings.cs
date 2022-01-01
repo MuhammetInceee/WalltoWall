31cf79b5ed609d02c499784a63927c2576e8dce2
MuhammetInce <muhammetinceee@gmail.com>
2022-01-01 17:34:48 +0300

    + Add : Scriptable Object For Ball

---
+++ Assets/Scripts/Ball_Settings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "WallToWall/Ball/Ball Settings")]
+public class Ball_Settings : ScriptableObject
+{
+    public float _jumpPowerHorizontal;
+    public float _jumpPowerVertical;
+}

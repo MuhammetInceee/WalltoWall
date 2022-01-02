625270100088e895fa2797bfe2e293c82ae590ba
MuhammetInce <muhammetinceee@gmail.com>
2022-01-02 13:51:12 +0300

    + Add : Ball Boundries

--- Assets/Scripts/Ball_Manager.cs
+++ Assets/Scripts/Ball_Manager.cs
@@ -7,35 +7,24 @@ public class Ball_Manager : MonoBehaviour
     [SerializeField] private Ball_Settings _ballController;
 
 
-    public Rigidbody2D _ballRb2D;
-
     public bool _ballGoRight = true;
+
+
+    private Rigidbody2D _ballRb2D;
+    private float _ballBoundary = 5.4f;
 
 
     void Start()
     {
-        _ballRb2D = gameObject.GetComponent<Rigidbody2D>();
+        _ballRb2D = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         BallControl();
+        BallBoundries();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Wall")
-        {
-            if (_ballGoRight)
-            {
-                _ballGoRight = false;
-            }
-            else
-            {
-                _ballGoRight = true;
-            }
-        }
-    }
 
 
     private void BallControl()
@@ -58,6 +47,42 @@ public class Ball_Manager : MonoBehaviour
     {
         _ballRb2D.velocity = new Vector2(0, 1) * _ballController._jumpPowerVertical * Time.deltaTime;
         _ballRb2D.AddForce(direction * _ballController._jumpPowerHorizontal * Time.deltaTime);
+    }
+
+    void BallBoundries()
+    {
+        if (this.gameObject.transform.position.y < -_ballBoundary)
+        {
+            //Game End Blocks
+            //Falls message on the end screen
+
+            //Just Try
+            Time.timeScale = 0f;
+            //Try End
+        }
+        else if (this.gameObject.transform.position.y > _ballBoundary)
+        {
+            //Game End Blocks
+            //Topside goes message on end screen
+
+            //Just Try
+            Time.timeScale = 0f;
+            //Try End
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            if (_ballGoRight)
+            {
+                _ballGoRight = false;
+            }
+            else
+            {
+                _ballGoRight = true;
+            }
+        }
     }
 
 }

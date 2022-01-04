da195538914099a419eea464dcf87738234a3e7f
MuhammetInce <muhammetinceee@gmail.com>
2022-01-04 16:04:12 +0300

    + Add : GameState Enum

---
+++ Assets/Scripts/GameStates.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStates : MonoBehaviour
+{
+    public enum GameState
+    {
+        PlayTime,
+        GameOver,
+        GamePaused
+    };
+
+    static GameState currentState;
+
+    private void Awake()
+    {
+        currentState = GameState.GamePaused;
+    }
+
+    public static void SetGameState(GameState newState, Rigidbody2D rb2D)
+    {
+        if (currentState == newState)
+            return;
+
+        currentState = newState;
+
+        switch (currentState)
+        {
+            case GameState.PlayTime:
+                rb2D.constraints = RigidbodyConstraints2D.None;
+                rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+                break;
+
+            case GameState.GamePaused:
+                rb2D.constraints = RigidbodyConstraints2D.FreezePosition;
+                break;
+
+            case GameState.GameOver:
+                rb2D.constraints = RigidbodyConstraints2D.FreezePosition;
+                break;
+        }
+    }
+}

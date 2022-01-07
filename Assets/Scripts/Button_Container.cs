424f783002b9f812cf2972db340ab4624f737833
MuhammetInce <muhammetinceee@gmail.com>
2022-01-07 15:04:26 +0300

    + Add : GameState on Restart Button

--- Assets/Scripts/Button_Container.cs
+++ Assets/Scripts/Button_Container.cs
@@ -8,13 +8,19 @@ public class Button_Container : MonoBehaviour
 
     [SerializeField] private GameObject _ball;
 
+
     public void RestartButton()
     {
+        Thorn_Spawn_Manager.Instance.AllThornsDeactivated();
+
+        if (!_ball.activeInHierarchy)
+            _ball.SetActive(true);
+
         _ball.transform.position = new Vector2(0, -1.89f);
         _ball.GetComponent<Ball_Manager>()._ballRb2D.velocity = new Vector2(0, 0);
         _ball.GetComponent<Ball_Manager>().score = 0;
         _ball.GetComponent<Ball_Manager>()._ballGoRight = true;
         _ball.GetComponent<Ball_Manager>().FirstTouch();
-        Thorn_Spawn_Manager.Instance.AllThornsDeactivated();
+        AbstractGameStates.SetGameState(AbstractGameStates.GameState.PlayTime, _ball.GetComponent<Rigidbody2D>());
     }
 }
